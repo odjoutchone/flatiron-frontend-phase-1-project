a9ff06c59a4e64e8d804c22319a367c8c43edbe0
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-06-25 12:21:53 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("connected")
+console.log("index.js connected")
+
+const handleFormInputFocus = () => {
+    console.log('focus accurred')
+};
+
+const searchTermsInput = document.body.querySelector("#search-terms")
+searchTermsInput.addEventListener("focus", handleFormInputFocus);
