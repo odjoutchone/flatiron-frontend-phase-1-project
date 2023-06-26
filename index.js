25005bb30f4ceecef3d15293bdc1f36c1d10647a
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-06-26 14:22:12 -0400

    fectch meal categories from themealdb Api

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ console.log("index.js connected");
 // get search field input
 const searchTermsInput = document.body.querySelector("#search-terms");
 
+// fectch meal categories from themealdb Api
 const getMealCategories = async () => {
-    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
     try{
-        const response = await fetch(mealCategoriesApiURL)
-        const data = response.json()
-        console.log(`data:`, data);
-    }
-    catch(error) {
-        alert("Something went wrong, try again later")
-
+        const response = await fetch(mealCategoriesApiURL);
+        const data = await response.json();
+        const categories = data.categories;
+        console.log(`categories: `, categories);
+        return categories;
+    }   catch(error) {
+        console.log(error);
+        alert("Something went wrong, try again later");
     }
 }
 
