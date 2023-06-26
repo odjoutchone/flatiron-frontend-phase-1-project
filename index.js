51000f6341a7c341b5fcc5dc862ad30035fb49d4
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-06-26 12:25:52 -0400

    Re-organize dom form input select& handleformfocus

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 console.log("index.js connected")
 
-const handleFormInputFocus = () => {
+const searchTermsInput = document.body.querySelector("#search-terms")
+
+const getMealCategories = async () => {
+    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+
+    try{
+        const response = await fetch(mealCategoriesApiURL)
+        const data = response.json()
+        console.log(`data:`, data);
+    }
+    catch(error) {
+        alert("Something went wrong, try again later")
+
+    }
+}
+
+const handleFormInputFocus = async () => {
     console.log('focus accurred')
+    await getMealCategories();
 };
 
-const searchTermsInput = document.body.querySelector("#search-terms")
+
 searchTermsInput.addEventListener("focus", handleFormInputFocus);
