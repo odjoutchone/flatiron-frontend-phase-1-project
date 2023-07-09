9838211c1722980774e07c5c66e47d2627841eeb
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-07-08 20:32:09 -0400

    add pseucode for renderMealCategories

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ const getMealCategories = async () => {
     }
 }
 
+// render meal categories data to dom
+const renderMealCategories = (mealCategoriesArray) => {
+    console.log('renderMealCategories');
+    console.table(mealCategoriesArray);
+// find and select a UL with meal categories ID dom element to append my data into
+
+// for each element in our meal categories array
+  // create a wrapping element maybe <article> class of card
+  // appendChild the article.card to select DOM element
+  // display the category name, image and description
+     // image
+     // create an img element
+        // set img src to category thumbnail url
+        // give class of category thumbnail
+        // give it a mobile friendly max width like 300px
+        // appendChild the image to our card
+     // name
+        // create an h4 element
+        // set h4 element textContent to be meal category name
+        // appendChild the h4 to our card
+    // description
+        // create an p element
+        // set p element textContent to be meal category description
+        // appendChild the p to our card
+}
+
 // function to call when the form input is giving focus
 const handleFormInputFocus = async () => {
     console.log('focus accurred');
