6cb6bc908a5784be97fb96670350c74da88de945
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-08-25 14:44:25 -0400

    index.html

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let getMeal = () => {
         console.log(myMeal.strMeal);
         console.log(myMeal.strMealThumb);
         console.log(myMeal.strInstructions);
-        
+
         // Resource Area
         let count = 1;
         let ingredients = [];
@@ -58,6 +58,7 @@ let getMeal = () => {
       });
   }
 };
+
 // Event Listeners
 window.addEventListener("load", getMeal);
 searchBtn.addEventListener("click", getMeal);
